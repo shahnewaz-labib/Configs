a2fb622cb4dcf665280ec5b2985b8fd13798f2a1
shahnewaz-labib <labib.bzs@gmail.com>
2021-03-01 16:04:56 +0600

    updated files on 2021-03-01 16:04:56

--- push.sh
+++ push.sh
@@ -6,4 +6,5 @@ cp /home/labib/.config/nvim/init.vim /home/labib/Configs/init.vim
 
 # Git add, commit, push
 git add .
-git commit -m "generated files on `date +'%Y-%m-%d %H:%M:%S'`"
+git commit -m "updated files on `date +'%Y-%m-%d %H:%M:%S'`"
+git push origin master
