958b9fe4726738dc9503d24554d663830b63a2a5
shahnewaz-labib <labib.bzs@gmail.com>
2021-03-23 20:48:24 +0600

    updated files on 2021-03-23 20:48:24

--- backup.sh
+++ backup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+
 sudo cp ~/codes/cp/template.cpp template.cpp
-echo "[+] copied cp code template"
+echo -e "${GREEN}[+] copied cp code template"
 
 sudo cp ~/.config/nvim/init.vim init.vim
 echo "[+] copied init.vim"
