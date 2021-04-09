aa24332048845dbc99f309bc25909ca0bd78d522
shahnewaz-labib <labib.bzs@gmail.com>
2021-04-09 21:52:24 +0600

    updated files on 2021-04-09 21:52:24

--- template.cpp
+++ template.cpp
@@ -1,27 +1,24 @@
-#include <bits/stdc++.h>
-#define lesgoo		ios::sync_with_stdio(0);cin.tie(0); cout.tie(0);
+#include "bits/stdc++.h"
+#define fastio		ios::sync_with_stdio(0);cin.tie(0); cout.tie(0);
 #define ll			long long
 #define pii 		pair<int, int>
 #define MIN(x,y)	(x) < (y) ? x : y
 #define MAX(x,y)	(x) > (y) ? x : y
 #define TC			int t;cin>>t;for(int tc=1;tc<=t;tc++)
 #define Unique(a)   sort(all(a)),a.erase(unique(all(a)),a.end
-#define yeet        return 0;
-#define nl          "\n"
 #define de(x)       cout << #x << " = " << x << "\n";
 #define HERE        cout << "HERE\n";
 #define precision(a) fixed << setprecision(a)
 #define endl '\n'
+
 int fx[]={+0,+0,+1,-1,-1,+1,-1,+1};
 int fy[]={-1,+1,+0,+0,+1,+1,-1,-1};
 using namespace std;
 
+const int mxn = 1e5+10;
+
 int main() {
-#ifndef ONLINE_JUDGE
-    // freopen("input.txt", "r", stdin);
-    // freopen("output.txt", "w", stdout);
-#endif
-    
+     
 
 
 
